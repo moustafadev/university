331805ab7f27c4129c56682ed0f0c3fe17d4b396
‪Mostafa Omar‬‏ <51782392+moustafadev@users.noreply.github.com>
2023-10-12 23:15:08 +0300

    first lab in ruby

--- labs/app/src/main/java/com/example/labs/MainActivity.kt
+++ labs/app/src/main/java/com/example/labs/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.labs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+<<<<<<< HEAD
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,10 +11,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+=======
+import androidx.compose.foundation.layout.fillMaxSize
+>>>>>>> 0ffd9e0 (hallo android)
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+<<<<<<< HEAD
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,6 +29,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+=======
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+>>>>>>> 0ffd9e0 (hallo android)
 import com.example.labs.ui.theme.LabsTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,13 +45,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+<<<<<<< HEAD
                     BirthdayCardPreview()
+=======
+                    Greeting("Android")
+>>>>>>> 0ffd9e0 (hallo android)
                 }
             }
         }
     }
 }
 
+<<<<<<< HEAD
 //@Composable
 //fun Greeting(name: String, modifier: Modifier = Modifier) {
 //    Text(
@@ -111,3 +125,20 @@ fun GreetingText(message: String, from: String, modifier: Modifier) {
         )
     }
 }
+=======
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LabsTheme {
+        Greeting("Android")
+    }
+}
+>>>>>>> 0ffd9e0 (hallo android)
